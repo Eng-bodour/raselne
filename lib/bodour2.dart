ca9e5f43f80a3afb43d756ecd8e8ebd8c45c2401
bodour majadmi <bodour.eng.28@gmail.com>
2022-08-02 14:35:50 +0300

    edit bodour2 edit

--- lib/bodour2.dart
+++ lib/bodour2.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Bodour2 extends StatelessWidget {
   const Bodour2({Key? key}) : super(key: key);
 
+  get() {}
   @override
   Widget build(BuildContext context) {
     return const Text('Bodour2');
