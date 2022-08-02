6cb9e7d9d479ea1776bef0a67d92ecae99bee24d
aya <aya.ghoury@gmail.com>
2022-08-02 15:04:37 +0300

    commit_ayanew5

--- lib/bodour2.dart
+++ lib/bodour2.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 class Bodour2 extends StatelessWidget {
   const Bodour2({Key? key}) : super(key: key);
 
   get() {}
   @override
   Widget build(BuildContext context) {
-    return const Text('Bodour2');
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: const Text('Bodour2'),
+    );
   }
 }
